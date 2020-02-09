fbd9d5a49a84121e92bbd6af7df06c16ac5f813e
Owen <owenedwards422@gmail.com>
2020-02-08 22:58:29 -0500

    new Date and found way to apply AM/PM in one loop

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+var calendar = $(".container");
+var time = new Date();
+console.log(time.getHours());
 
+for (var i = 9; i < 18; i++) {
+    var x = i;
+    if (x > 12) {
+        x = x - 12
+        console.log("the time is: " + x + " PM")
+    } else {
+        console.log("the time is: " + x + " AM")
+    }
+}
